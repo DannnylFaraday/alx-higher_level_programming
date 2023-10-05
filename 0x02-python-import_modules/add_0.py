1ca70a49bb91ed61ba35c0ef23f5ecb68b930caf
Ayomide Adeosun <ayomide.daniel.adeosun@gmail.com>
2023-10-05 14:26:28 -0700

    module

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """This is an addition function
+
+    Arguments:
+        (a) is the first integer
+        (b) is the second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
