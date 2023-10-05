85f41e8cf2abf8ed7f8600473adf971c9ad85487
Ayomide Adeosun <ayomide.daniel.adeosun@gmail.com>
2023-10-05 14:45:25 -0700

    calc_1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+def add(a, b):
+    """This is an addition function
+
+    Arguments:
+        (a) is the first integer
+        (b) is the second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+def sub(a, b):
+    """This is a subtractionfunction
+
+    Arguments:
+        (a) is the first integer
+        (b) is the second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+def multi(a, b):
+    """This is a multiplication function
+
+    Arguments:
+        (a) is the first integer
+        (b) is the second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """This is a division function
+
+    Arguments:
+        (a) is the first integer
+        (b) is the second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return (a / b)
